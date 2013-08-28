ea93c0216b9ed0ee235fd2f52a406a81b9a70001
Akshar Raaj <akshar@agiliq.com>
2013-08-28 19:00:51 +0530

    Added development settings file, it will have django_debug_toolbar, django-extensions etc.

---
+++ klpmis/klpmis/dev_settings.py
@@ -0,0 +1,12 @@
+from .local_settings import *
+
+INSTALLED_APPS += (
+    'django_extensions',
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
