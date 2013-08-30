8d52502e2467dff920c3c5c85a74299e02f46080
Akshar Raaj <akshar@agiliq.com>
2013-08-30 22:06:54 +0530

    Default permitted_methods is GET only

--- klpmis/utils/urls.py
+++ klpmis/utils/urls.py
@@ -4,6 +4,6 @@ from .views import set_session
 from klprestApi.HomeApi import KLP_Home
 
 urlpatterns = patterns('',
-    url(r'^home/$', KLP_Home(permitted_methods=('GET',)), name='home'),
+    url(r'^home/$', KLP_Home(), name='home'),
     url(r'^set/session/$', set_session, name='set_session')
 )
