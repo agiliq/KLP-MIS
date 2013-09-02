9ed45bcbbb146a8fd452827e674dfea0b734c077
krvc <krvc.it@gmail.com>
2013-09-03 00:49:59 +0530

    initial restructure commit

--- manage.py
+++ manage.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'klpmis.settings.base'
-                          )
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "klpmis.settings.base")
 
     from django.core.management import execute_from_command_line
 
