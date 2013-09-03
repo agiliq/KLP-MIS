e6229f8b0fe3a5e74e5282cfb3013da985fb2aa5
krvc <krvc.it@gmail.com>
2013-09-03 15:43:08 +0530

    Changed the path of dev_settings

---
+++ klpmis/settings/dev_settings.py
@@ -0,0 +1,16 @@
+from .local_settings import *
+
+INSTALLED_APPS += (
+    'django_extensions',
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False
+}
