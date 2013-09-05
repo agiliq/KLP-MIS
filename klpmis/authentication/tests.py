06e26610513e60335306fcce0a81c6128cd2ae82
krvc <krvc.it@gmail.com>
2013-09-06 00:11:18 +0530

    Added tests for authentication

--- klpmis/authentication/tests.py
+++ klpmis/authentication/tests.py
@@ -1,16 +1,40 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestViewsBasic(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="foo",
+                                             email="foo@example.com",
+                                             password="bar")
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(302, response.status_code)
+        self.client.login(username="foo", password="bar")
+        response = self.client.get(reverse("index"))
+        self.assertEqual(302, response.status_code)
+
+    def test_login(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(200, response.status_code)
+        self.client.login(username="foo", password="bar")
+        response = self.client.get(reverse("login"))
+        self.assertEqual(302, response.status_code)
+
+    def test_logout(self):
+        response = self.client.get(reverse("logout"))
+        self.assertEqual(302, response.status_code)
+        self.client.login(username="foo", password="bar")
+        response = self.client.get(reverse("logout"))
+        self.assertEqual(302, response.status_code)
+
+    def test_klp_user_auth(self):
+        response = self.client.get(reverse("klp_user_auth"))
+        self.assertEqual("False", response.content)
+        self.client.login(username="foo", password="bar")
+        response = self.client.get(reverse("klp_user_auth"))
+        self.assertEqual("True", response.content)
