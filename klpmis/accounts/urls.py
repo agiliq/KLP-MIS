d19740030e1f91ed368359e9ddef34ca3daf4c55
krvc <krvc.it@gmail.com>
2013-09-06 18:30:14 +0530

    Added the klprestApi urls to accounts

--- klpmis/accounts/urls.py
+++ klpmis/accounts/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, url
 
-from .views import login, logout, klp_user_auth, index
+from .views import *
 
 urlpatterns = patterns('',
     url(r'^$', index, name="index"),
     url(r'^login/?$', login, name="login"),
     url(r'^logout/?$', logout, name="logout"),
-    url(r'^user/authentication/?$', klp_user_auth, name="klp_user_auth"),
+    url(r'^user/authentication/?$', user_auth, name="user_auth"),
+    url(r'^accounts/auth/user/add/$', AddUser, name='accounts_add_user'),
+    url(r'^accounts/auth/user/addNewUser_done/$', AddUserDone, name='accounts_add_user_done'),
+    url(r'^accounts/password/change/$', ChangePassword, name='accounts_password_change'),
+    url(r'^accounts/password/done/$', ChangePasswordDone, name='accounts_password_change_done'),
 )
