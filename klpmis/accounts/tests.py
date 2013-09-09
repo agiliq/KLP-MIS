c6fbde795c2cd3d800a2bc1a6b1aef15de29bde4
krvc <krvc.it@gmail.com>
2013-09-10 01:22:06 +0530

    Added tests for add_user

--- klpmis/accounts/tests.py
+++ klpmis/accounts/tests.py
@@ -2,13 +2,22 @@ from django.test import TestCase
 from django.test import Client
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 
 class TestViewsBasic(TestCase):
     def setUp(self):
-        self.user = User.objects.create_superuser(username="foo",
-                                             email="foo@example.com",
-                                             password="bar")
+        test_group_1 = Group(name='test_group_1')
+        test_group_1.save()
+        test_group_2 = Group(name='test_group_2')
+        test_group_2.save()
+        test_group_3 = Group(name='test_group_3')
+        test_group_3.save()
+
+        self.user = \
+            User.objects.create_superuser(username="foo",
+                                          email="foo@example.com",
+                                          password="bar")
         self.client = Client()
 
     def test_index(self):
@@ -32,9 +41,28 @@ class TestViewsBasic(TestCase):
         response = self.client.get(reverse("logout"))
         self.assertEqual(302, response.status_code)
 
-    def test_klp_user_auth(self):
-        response = self.client.get(reverse("klp_user_auth"))
+    def test_user_auth(self):
+        response = self.client.get(reverse("user_auth"))
         self.assertEqual("False", response.content)
         self.client.login(username="foo", password="bar")
-        response = self.client.get(reverse("klp_user_auth"))
+        response = self.client.get(reverse("user_auth"))
         self.assertEqual("True", response.content)
+
+    def test_add_user_without_login(self):
+        form_data = {'username': 'test1',
+                     'password1': 'test1',
+                     'password2': 'test1',
+                     'groups': [1, 2, 3]}
+        response = self.client.post(reverse('accounts_add_user'),
+                                    form_data)
+        self.assertEqual(302, response.status_code)
+
+    def test_add_user_with_login(self):
+        form_data = {'username': 'test1',
+                     'password1': 'test1',
+                     'password2': 'test1',
+                     'groups': [1, 2, 3]}
+        self.client.login(username="foo", password="bar")
+        response = self.client.post(reverse('accounts_add_user'),
+                                    form_data)
+        self.assertEqual(302, response.status_code)
