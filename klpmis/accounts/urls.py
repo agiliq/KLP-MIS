7f702ef3bcd6f57abcf1aafc826c7e99eeb2aea2
Akshar Raaj <akshar@agiliq.com>
2013-09-12 16:32:01 +0530

    Why ? after urlpattern

--- klpmis/accounts/urls.py
+++ klpmis/accounts/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = patterns('',
     url(r'^$', index, name="index"),
-    url(r'^login/?$', login, name="login"),
-    url(r'^logout/?$', logout, name="logout"),
-    url(r'^user/authentication/?$', user_auth, name="user_auth"),
+    url(r'^login/$', login, name="login"),
+    url(r'^logout/$', logout, name="logout"),
+    url(r'^user/authentication/$', user_auth, name="user_auth"),
     url(r'^accounts/auth/user/add/$', add_user, name='accounts_add_user'),
     url(r'^accounts/auth/user/addNewUser_done/$', add_user_done, name='accounts_add_user_done'),
     url(r'^accounts/password/change/$', change_password, name='accounts_password_change'),
