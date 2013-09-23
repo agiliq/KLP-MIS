3631aae28187b6094433334d3b8f07b3fa8d2e86
krvc <krvc.it@gmail.com>
2013-09-23 19:58:35 +0530

    Pep8 compliant

--- klpmis/klprestApi/QuestionApi.py
+++ klpmis/klprestApi/QuestionApi.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-QuestionApi is used 
+QuestionApi is used
 1) To view Individual Question details.
 2) To create new Question
 3) To update existing Question
 """
 
 from django.conf.urls.defaults import *
-from django_restapi.resource import Resource
 from django.http import HttpResponse
+
 from schools.models import *
 from schools.forms import *
-from django_restapi.model_resource import Collection, Entry
+from django_restapi.model_resource import Collection
 from django_restapi.responder import *
 from django_restapi.receiver import *
 from klprestApi.BoundaryApi import ChoiceEntry
@@ -40,14 +40,17 @@ def KLP_Question_View(request, question_id):
     resp = \
         KLP_Question(queryset=Question.objects.filter(pk=question_id),
                      permitted_methods=('GET', 'POST'),
-                     responder=TemplateResponder(template_dir='viewtemplates'
-                     , template_object_name='question'))(request,
-            question_id, **kwrg)
+                     responder=TemplateResponder(
+                         template_dir='viewtemplates',
+                         template_object_name='question'))(request,
+                                                           question_id,
+                                                           **kwrg)
     return HttpResponse(resp)
 
 
 def KLP_Question_Create(request, referKey):
-    """ To Create New Question assessment/question/(?P<referKey>\d+)/creator/"""
+    """ To Create New Question
+            assessment/question/(?P<referKey>\d+)/creator/"""
 
     # Checking user Permissions for Question add
 
@@ -63,19 +66,23 @@ def KLP_Question_Create(request, referKey):
     KLP_Create_Question = \
         KLP_Question(queryset=Question.objects.filter(pk=0),
                      permitted_methods=('GET', 'POST'),
-                     responder=TemplateResponder(template_dir='viewtemplates'
-                     , template_object_name='question',
-                     extra_context={'buttonType': buttonType,
-                     'referKey': referKey, 'order': order}),
+                     responder=TemplateResponder(
+                         template_dir='viewtemplates',
+                         template_object_name='question',
+                         extra_context={'buttonType': buttonType,
+                                        'referKey': referKey,
+                                        'order': order}),
                      receiver=XMLReceiver())
-    response = KLP_Create_Question.responder.create_form(request,
-            form_class=Question_Form)
+    response = \
+        KLP_Create_Question.responder.create_form(request,
+                                                  form_class=Question_Form)
 
     return HttpResponse(response)
 
 
 def KLP_Question_Update(request, question_id):
-    """ To update Selected Question question/(?P<question_id>\d+)/update/"""
+    """ To update Selected Question
+            question/(?P<question_id>\d+)/update/"""
 
     # Checking user Permissions for Question Update
 
@@ -91,20 +98,24 @@ def KLP_Question_Update(request, question_id):
     KLP_Edit_Question = \
         KLP_Question(queryset=Question.objects.filter(pk=question_id),
                      permitted_methods=('GET', 'POST'),
-                     responder=TemplateResponder(template_dir='edittemplates'
-                     , template_object_name='question',
-                     extra_context={'buttonType': buttonType,
-                     'referKey': referKey}), receiver=XMLReceiver())
-    response = KLP_Edit_Question.responder.update_form(request,
-            pk=question_id, form_class=Question_Form)
+                     responder=TemplateResponder(
+                         template_dir='edittemplates',
+                         template_object_name='question',
+                         extra_context={'buttonType': buttonType,
+                                        'referKey': referKey}),
+                     receiver=XMLReceiver())
+    response = \
+        KLP_Edit_Question.responder.update_form(request,
+                                                pk=question_id,
+                                                form_class=Question_Form)
 
     return HttpResponse(response)
 
 
 urlpatterns = patterns('',
-                       url(r'^assessment/question/(?P<referKey>\d+)/creator/?$'
-                       , KLP_Question_Create),
+                       url(r'^assessment/question/(?P<referKey>\d+)/creator/?$',
+                           KLP_Question_Create),
                        url(r'^question/(?P<question_id>\d+)/view/?$',
-                       KLP_Question_View),
+                           KLP_Question_View),
                        url(r'^question/(?P<question_id>\d+)/update/?$',
-                       KLP_Question_Update))
+                           KLP_Question_Update))
