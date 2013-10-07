57c3ea766755325aacccda04278b7d49e33c19ab
krvc <krvc.it@gmail.com>
2013-10-07 20:29:48 +0530

    Pep8 compliant

--- klpmis/schools/management/commands/KLP_assignPermissions.py
+++ klpmis/schools/management/commands/KLP_assignPermissions.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from django.core.management.base import BaseCommand, CommandError
-from django.core.mail import send_mail
-from klpmis.settings import PROJECT_NAME, PROJECT_ROOT
-from django.conf import settings
-
-from schools.models import *
 import django
 import datetime
 import os
 import csv
-from klprestApi.KLP_Permission import assignPermission
-from django.contrib.contenttypes.models import ContentType
 from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.db import transaction
 
+from klpmis.settings import PROJECT_NAME, PROJECT_ROOT
+from klprestApi.KLP_Permission import assignPermission
+from schools.models import *
 
-def permission_status_message(assessList,assId,index_val):
+
+def permission_status_message(assessList, assId, index_val):
     if assessList[assId][index_val]:
         perm_list = str(assessList[assId][index_val])
         msg += '\t \t \t No of Institution : ' \
-                            + str(len(assessList[assId][index_val])) \
-                            + ''': 
+            + str(len(assessList[assId][index_val])) \
+            + ''':
 
       \t \t \t ''' \
                             + perm_list[1:len(perm_list) - 1] \
@@ -32,9 +33,12 @@ def permission_status_message(assessList,assId,index_val):
         msg = ' \t \t \t None . \n\n'
     return msg
 
+
 class Command(BaseCommand):
 
-        # option_list = BaseCommand.option_list + (make_option('--user', dest='user', type='string',help='User running the command'),)
+        # option_list = BaseCommand.option_list +
+        # (make_option('--user', dest='user', type='string',
+            # help='User running the command'),)
 
     @transaction.autocommit
     def handle(self, *args, **options):
@@ -43,7 +47,7 @@ class Command(BaseCommand):
 
             # Reads the arguments from command line.
 
-            print 'INNTER TRY',len(args)
+            print 'INNTER TRY', len(args)
             inst_list = args[0]  # options["inst_list"]
             deUserList = args[1]  # options["deUserList"]
 
@@ -53,9 +57,9 @@ class Command(BaseCommand):
             assessmentPerm = args[5]  # options["assessmentPerm"]
             bound_cat = args[6]
             bound_list = args[7]
-            username =args[8]
-            current_user= args[9]
-            path_info=args[10] 
+            username = args[8]
+            current_user = args[9]
+            path_info = args[10]
             inst_listall = []
             deUserList = deUserList.split(',')
             permissions = permissions.split(',')
@@ -67,26 +71,29 @@ class Command(BaseCommand):
                 inst_listall = inst_list
             else:
 
-                            # asmIdList = assignPermission(inst_list, deUserList, permissions, permissionType, assessmentId, assessmentPerm)
+            # asmIdList = assignPermission(inst_list, deUserList,
+                # permissions, permissionType, assessmentId, assessmentPerm)
 
                 if bound_cat == 'district':
                     inst_listall = \
-                        Institution.objects.filter(boundary__parent__id__in=bound_list,
+                        Institution.objects.filter(
+                            boundary__parent__id__in=bound_list,
                             active=2).values_list('id',
-                            flat=True).distinct()
+                                                  flat=True).distinct()
                 elif bound_cat in ['block', 'project']:
                     inst_listall = \
-                        Institution.objects.filter(boundary__id__in=bound_list,
+                        Institution.objects.filter(
+                            boundary__id__in=bound_list,
                             active=2).values_list('id',
-                            flat=True).distinct()
+                                                  flat=True).distinct()
             (asmIdList, allassignIds, dic) = assignPermission(
                 inst_listall,
                 deUserList,
                 permissions,
                 permissionType,
                 assessmentId,
-                assessmentPerm,username,current_user,path_info
-                )
+                assessmentPerm, username, current_user, path_info
+            )
             self.SendingMail(
                 asmIdList,
                 deUserList,
@@ -97,10 +104,11 @@ class Command(BaseCommand):
                 bound_list,
                 username,
                 dic,
-                )
+            )
             print 'Successfully Assigned'
         if 0:
-            raise CommandError('Pass First Parameter as Boundary Ids List file and Second Parameter as User Ids List \n'
+            raise CommandError('Pass First Parameter as Boundary Ids List \
+                file and Second Parameter as User Ids List \n'
                                )
 
     def SendingMail(
@@ -114,52 +122,45 @@ class Command(BaseCommand):
         bound_list,
         username,
         dic,
-        ):
+    ):
         inst_list = list(set(inst_list))
         detailmessage = \
-            'The following is the Assignment data for the current action only:\n'
+            'The following is the Assignment data for the\
+             current action only:\n'
         for k in dic:
 
-            detailmessage += '''User name :%s (%d) 
- 
- ''' % (k[0],
-                    k[1])
+            detailmessage += '''User name :%s (%d)''' % (k[0], k[1])
             assIds = dic[k][0]
             for assId in assIds:
                 assName = Assessment.objects.filter(id=assId)[0].name
                 permission_update_status()
                 detailmessage += \
-                    ''' \t \t Permissions were already assigned to the following institutions for assessment %s :
- 
-''' \
+                    ''' \t \t Permissions were already assigned to the \
+                    following institutions for assessment %s :''' \
                     % (str(assId) + '-' + assName)
-                detailmessage += permission_status_message(assIds,assId,0)
+                detailmessage += permission_status_message(assIds, assId, 0)
 
                 detailmessage += \
-                    ''' \t \t Permissions were newly assigned to the following institutions for assessment %s :
- 
- ''' \
+                    ''' \t \t Permissions were newly
+                    assigned to the following \
+                    institutions for assessment %s :''' \
                     % (str(assId) + '-' + assName)
-                
-                detailmessage += permission_status_message(assIds,assId,1)
+
+                detailmessage += permission_status_message(assIds, assId, 1)
             if not assIds:
                 detailmessage += \
-                    '''\t \t Assessment assigned to the user: None 
-
-'''
+                    '''\t \t Assessment assigned to the user: None'''
             if dic[k][1] or dic[k][2]:
                 detailmessage += \
-                    ''' \t \t Permissions were newly assigned to the following institutions 
+                    ''' \t \t Permissions were newly assigned
+                    to the following institutions'''
 
-'''
-
-                detailmessage += permission_status_message(dic,k,1)
+                detailmessage += permission_status_message(dic, k, 1)
 
                 detailmessage += \
-                    ''' \t \t Permissions were already assigned to the following institutions
-
- '''
-                detailmessage += permission_status_message(dic,k,2)
+                    ''' \t \t Permissions were already assigned to the
+                    following institutions'''
+                detailmessage += permission_status_message(dic, k, 2)
 
         inst_liststr = ', '.join(str(x) for x in inst_list)
         boundarystr = ''
@@ -175,20 +176,17 @@ class Command(BaseCommand):
         sender = settings.REPORTMAIL_SENDER
         receiver = settings.REPORTMAIL_RECEIVER
         subject = \
-            'Assigned Permissions for Institutions and Assessments by %s from %s server' \
+            'Assigned Permissions for Institutions and \
+            Assessments by %s from %s server' \
             % (username, PROJECT_NAME)
         permissionTypetext = ('Assign Permissions' if permissionType
                               == 'permissions'
-                               else 'Assessment Permissions')
+                              else 'Assessment Permissions')
         assessmentPermtext = ('True' if assessmentPerm not in ['None',
                               None, ''] else 'False')
         fullmsg = \
-            '''%s 
- %s  
- User List : %s  
- Admin Action :%s 
- Assessment Id List : %s 
- Assign Assessment checked : %s ''' \
+            '''%s%s User List : %s Admin Action :%s Assessment Id List : %s
+            Assign Assessment checked : %s ''' \
             % (
             boundarystr,
             detailmessage,
@@ -196,8 +194,6 @@ class Command(BaseCommand):
             permissionTypetext,
             assessmentId,
             assessmentPermtext,
-            )
+        )
 
         send_mail(subject, fullmsg, sender, receiver)
-
-
