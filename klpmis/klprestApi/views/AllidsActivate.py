b5968cbe43a5bccc4ca710ff975dd4c1743dc8db
krvc <krvc.it@gmail.com>
2013-10-12 00:05:41 +0530

    Modified AllidsActive

--- klpmis/klprestApi/views/AllidsActivate.py
+++ klpmis/klprestApi/views/AllidsActivate.py
@@ -10,11 +10,12 @@ Institution Api is used
 """
 import simplejson
 
+from django.core.mail import send_mail
+from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 
-# from klpmis.settings import Boundary, Institution, Programme, Assessment, Question, Student, Staff, StudentGroup
 from schools.models import *
 from schools.forms import *
 from klpmis.settings import *
@@ -151,3 +152,56 @@ def KLP_Activation(request):
 
     return HttpResponse(simplejson.dumps(respDict),
                         content_type='application/json; charset=utf-8')
+
+
+def hasChildObj(idlists, model_name1):
+    modelDict = {
+        'boundary': Boundary,
+        'institution': Institution,
+        'programme': Programme,
+        'assessment': Assessment,
+        'question': Question,
+        'studentgroup': StudentGroup,
+        'student': Student,
+        'staff': Staff,
+        'class': StudentGroup,
+        'center': StudentGroup,
+    }
+
+        # Checking user Permissions
+
+    haschildlist = []
+    for k in idlists:
+
+          # Get Object based on id and model to delete
+
+        obj = modelDict[model_name1.lower()].objects.get(pk=k)
+        if model_name1.lower() == 'boundary':
+            flag = obj.getChild(obj.boundary_type)
+        elif model_name1.lower() in ['class', 'studentgroup']:
+            if Student_StudentGroupRelation.objects.filter(
+                student_group__id=k, active=2,
+                    academic=current_academic()).count():
+                flag = True
+            else:
+                flag = False
+        else:
+            flag = obj.getChild()
+
+        if flag:
+            haschildlist.append(str(k))
+
+    return haschildlist
+
+
+def SendingMail(idlist, mname, atype):
+    inst_liststr = idlist
+    sender = settings.REPORTMAIL_SENDER
+    receiver = settings.REPORTMAIL_RECEIVER
+    if atype == "Deactivated":
+        subject = 'Deactivated list'
+        fullmsg = 'Following %s Ids are Deactivated  :  \n %s ' % (mname, inst_liststr)
+    else:
+        subject = 'Activated list'
+        fullmsg = 'Following %s Ids are Activated  :  \n %s ' % (mname, inst_liststr)
+    send_mail(subject, fullmsg, sender, receiver)
