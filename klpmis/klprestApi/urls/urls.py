58bc9ed5bcbb7991ef32070948c49f33523a97d9
krvc <krvc.it@gmail.com>
2013-10-12 02:01:49 +0530

    Updated ChildApi urls

--- klpmis/klprestApi/urls/urls.py
+++ klpmis/klprestApi/urls/urls.py
@@ -12,6 +12,8 @@ from klprestApi.views.BoundryApi import KLP_Boundary_View,\
     KLP_Boundary_Create, KLP_Boundary_Update
 from klprestApi.views.BoundaryTypeApi import \
     template_boundary_type_view
+from klprestApi.views.ChildApi import KLP_Child_Create,\
+    KLP_Child_View, KLP_Child_Update, childsql, ChildrenList, StdGrpFilter
 
 
 urlpatterns = patterns('', url(r'^KLP_activaterecords/$', KLP_Activation),
@@ -46,5 +48,13 @@ urlpatterns = patterns('', url(r'^KLP_activaterecords/$', KLP_Activation),
                        url(r'^boundary/creator/?$', KLP_Boundary_Create),
                        url(r'^boundary/(?P<boundary_id>\d+)/update/$', KLP_Boundary_Update),
                        url(r'^boundary-type/creator/?$',
-                       template_boundary_type_view.responder.create_form,
-                       {'form_class': 'boundary_type'}))
+                           template_boundary_type_view.responder.create_form,
+                       {'form_class': 'boundary_type'}),
+                       url(r'^boundary/(?P<referKey>.*)/child/creator/$',
+                           KLP_Child_Create),
+                       url(r'^child/(?P<child_id>\d+)/view/?$', KLP_Child_View),
+                       url(r'^child/(?P<child_id>\d+)/update/?$', KLP_Child_Update),
+                       url(r'^boundary/(?P<boundary_id>\d+)/child/view/$', ChildrenList),
+                       url(r'^filter/(?P<school_id>\d+)/schgrp/$',
+                           StdGrpFilter(permitted_methods=('POST', 'GET'))),
+                       url(r'^childsql/(?P<boundary_id>\d+)/$', childsql),)
