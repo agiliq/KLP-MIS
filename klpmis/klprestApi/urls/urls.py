fc1dac88223818a5355d458d0de1606df8a13ba1
krvc <krvc.it@gmail.com>
2013-10-12 00:04:56 +0530

    Added AnswersApi urls

--- klpmis/klprestApi/urls/urls.py
+++ klpmis/klprestApi/urls/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls.defaults import patterns, url
+
 from klprestApi.views.AllidsActivate import KLP_Activation, KLP_act_form
+from klprestApi.views.AnswersApi import KLP_DataEnry, KLP_DataValidation, KLP_getAnswers
 
 urlpatterns = patterns('', url(r'^KLP_activaterecords/$',
                        KLP_Activation),
                        url(r'^KLP_activaterecords_form/$',
-                       KLP_act_form))
+                       KLP_act_form),
+                       url(r'^answer/data/entry/$', KLP_DataEnry),
+                       url(r'^answer/data/validation/$', KLP_DataValidation),
+                       url(r'^klp/getanswers/$', KLP_getAnswers))
