27c1b77d91d132527a2adb7720abd8b7a69c58fa
krvc <krvc.it@gmail.com>
2013-10-12 18:48:53 +0530

    Added KLP_Map to views

---
+++ klpmis/klprestApi/views/KLP_Map.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+
+from vendor.django_restapi.responder import *
+from vendor.django_restapi.receiver import *
+from schools.models import *
+
+
+def KLP_Map_SG(request):
+    """ This method uses to map Student Groups With Assessment"""
+
+    boundary_id = request.GET.get('boundary')
+    asssessment_id = request.GET.get('assessment')
+    assessmentObj = Assessment.objects.get(id=asssessment_id)
+    studentgroup_list = \
+        StudentGroup.objects.filter(
+            institution__boundary__parent__parent__id=boundary_id)
+    for sg in studentgroup_list:
+        sg_as_mapObj = \
+            Assessment_StudentGroup_Association(assessment=assessmentObj,
+                                                student_group=sg, active=2)
+        sg_as_mapObj.save()
+    return HttpResponse(studentgroup_list)
