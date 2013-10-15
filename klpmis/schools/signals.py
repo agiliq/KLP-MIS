58cdbaca682c030f1add7f3d73d15a4cfdaf7f3d
krvc <krvc.it@gmail.com>
2013-10-16 01:17:51 +0530

    Pep8 Compliant

--- klpmis/schools/signals.py
+++ klpmis/schools/signals.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""This File Contains the Signal definations. These signals 
+"""This File Contains the Signal definations. These signals
 are used to check permissions of user """
 
 from django.dispatch import Signal
 
 check_perm = Signal(providing_args=['user', 'instance', 'Assessment',
-                    'permission'])  
+                    'permission'])
                     # This Signal used to check user object level permissions
 
-check_user_perm = Signal(providing_args=['user', 'model', 'operation'])  
-# This Signal used to check user operational 
+check_user_perm = Signal(providing_args=['user', 'model', 'operation'])
+# This Signal used to check user operational
 #permissions (add/update/delete permissions)
-
