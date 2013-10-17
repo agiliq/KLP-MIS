de1d53849994973a55479320698775dfcde4bab1
krvc <krvc.it@gmail.com>
2013-10-17 18:47:17 +0530

    MOdified imports in StudentApi

--- klpmis/klprestApi/views/StudentApi.py
+++ klpmis/klprestApi/views/StudentApi.py
@@ -9,11 +9,11 @@ StudentApi is used
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-from schools.models import *
-from schools.forms import *
+from schools.models import Student, StudentGroup, Institution, Child
+from schools.forms import Child_Form
 from vendor.django_restapi.model_resource import Collection
-from vendor.django_restapi.responder import *
-from vendor.django_restapi.receiver import *
+from vendor.django_restapi.responder import TemplateResponder
+from vendor.django_restapi.receiver import XMLReceiver
 from klprestApi.views.BoundaryApi import ChoiceEntry
 from schools.receivers import KLP_user_Perm
 
