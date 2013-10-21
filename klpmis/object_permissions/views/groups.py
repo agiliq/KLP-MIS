36ce4b60ba0d2b7b8a585e8caaf692a891ccb4a5
krvc <krvc.it@gmail.com>
2013-10-22 01:19:31 +0530

    Use render

--- klpmis/object_permissions/views/groups.py
+++ klpmis/object_permissions/views/groups.py
@@ -4,8 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseForbidden
-from django.shortcuts import get_object_or_404, render_to_response
-from django.template import RequestContext
+from django.shortcuts import get_object_or_404, render
 
 from object_permissions import get_user_perms
 from object_permissions.signals import view_edit_user
@@ -16,65 +15,68 @@ from object_permissions.views.permissions import ObjectPermissionForm
 def user_permissions(request, id, user_id=None):
     """
     Ajax call to update a user's permissions
-    
+
     @param id: id of Group
     """
     editor = request.user
     group = get_object_or_404(Group, id=id)
-    
+
     if not (editor.is_superuser or editor.has_perm('admin', group)):
         return HttpResponseForbidden('You do not have sufficient privileges')
-    
+
     if request.method == 'POST':
         form = ObjectPermissionForm(Group, request.POST)
         if form.is_valid():
             form.update_perms()
             user = form.cleaned_data['user']
-            
+
             # send signal
             view_edit_user.send(sender=editor, user=user, obj=group)
-            
+
             # return html to replace existing user row
             url = reverse('group-permissions', args=[id])
-            return render_to_response(
-                "object_permissions/muddle/group/user_row.html",
-                {'object':group, 'user_detail':user, 'url':url},
-                context_instance=RequestContext(request))
-        
+            context = {'object': group, 'user_detail': user, 'url': url}
+            return render(request,
+                          "object_permissions/muddle/group/user_row.html",
+                          context)
+
         # error in form return ajax response
         content = json.dumps(form.errors)
         return HttpResponse(content, mimetype='application/json')
-    
+
     # render a form for an existing user only
     form_user = get_object_or_404(User, id=user_id)
-    data = {'permissions':get_user_perms(form_user, group),
-            'obj':group, 'user':user_id}
+    data = {'permissions': get_user_perms(form_user, group),
+            'obj': group, 'user': user_id}
     form = ObjectPermissionForm(Group, data)
-    return render_to_response("object_permissions/permissions/form.html",
-                {'form':form, 'obj':group, 'user_id':user_id,
-                'url':reverse('group-permissions', args=[group.id])},
-                context_instance=RequestContext(request))
-    
+    context = {'form': form, 'obj': group, 'user_id': user_id,
+               'url': reverse('group-permissions', args=[group.id])}
+    return render(request,
+                  "object_permissions/permissions/form.html",
+                  context)
+
 
 @login_required
-def all_permissions(request, id, \
-                    template='object_permissions/permissions/objects.html', rest=False):
+def all_permissions(request, id,
+                    template='object_permissions/permissions/objects.html',
+                    rest=False):
     """
     Generic view for displaying permissions on all objects.
-    
+
     @param id: id of group
     @param template: template to render the results with, default is
     permissions/objects.html
     """
     user = request.user
     group = get_object_or_404(Group, pk=id)
-    
+
     if not (user.is_superuser or group.user_set.filter(pk=user.pk).exists()):
         if not rest:
-            return HttpResponseForbidden('You do not have sufficient privileges')
+            return HttpResponseForbidden('You do not have \
+                    sufficient privileges')
         else:
-            return {'error':'You do not have sufficient privileges'}
-    
+            return {'error': 'You do not have sufficient privileges'}
+
     perm_dict = group.get_all_objects_any_perms()
 
     # exclude group permissions from this view, they are treated special
@@ -91,9 +93,7 @@ def all_permissions(request, id, \
         repacked[cls.__name__] = objs
 
     if not rest:
-        return render_to_response(template, \
-            {'persona':group, 'perm_dict':repacked}, \
-        context_instance=RequestContext(request),
-    )
+        context = {'persona': group, 'perm_dict': repacked}
+        return render(request, template, context)
     else:
-        return {'persona':group, 'perm_dict':repacked}
+        return context
