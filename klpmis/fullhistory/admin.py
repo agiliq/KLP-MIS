f3cccd099dd508b03fc91a75bed177cd1cdd9f81
krvc <krvc.it@gmail.com>
2013-11-27 14:07:48 +0530

    Modified import in admin.py

--- klpmis/fullhistory/admin.py
+++ klpmis/fullhistory/admin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 import fullhistory
 import views
